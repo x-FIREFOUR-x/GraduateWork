4840f8e760dcf935c6b7616df0e8f3562c2d1a42
Alex <olexandrpasalsky@gmail.com>
2022-12-07 13:24:51 +0200

    Enemy, move Enemy (WavePoint)

---
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+    void Start()
+    {
+        target = WayPoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        wavePointIndex++;
+        if (wavePointIndex > WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            target = WayPoints.points[wavePointIndex];
+        }
+    }
+}
