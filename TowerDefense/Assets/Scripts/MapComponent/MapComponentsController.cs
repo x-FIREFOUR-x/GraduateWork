b365fb1438e197c678b78da4f6ecad21df863742
Alex <olexandrpasalsky@gmail.com>
2022-12-12 17:25:51 +0200

    class PathGenerator (so bad) (bug)

--- TowerDefense/Assets/Scripts/MapComponent/MapComponentsController.cs
+++ TowerDefense/Assets/Scripts/MapComponent/MapComponentsController.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -21,14 +20,15 @@ public class MapComponentsController : MonoBehaviour
 
     [Header("Map Attributes")]
     [SerializeField]
-    Vector2Int indexesStart = new Vector2Int(1, 1);
+    Vector2Int IndexesStart = new Vector2Int(1, 1);
     [SerializeField]
-    Vector2Int indexesEnd = new Vector2Int(14, 14);
+    Vector2Int IndexesEnd = new Vector2Int(14, 14);
     [SerializeField]
     private int Size = 16;
 
     void Start()
     {
+        /*
         List<List<int>> generatedTilesMatrix = new List<List<int>> {
             new List<int>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
             new List<int>{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
@@ -66,8 +66,9 @@ public class MapComponentsController : MonoBehaviour
                 }
             }
         }
+        */
         
-        //List<Vector2Int> generatedPath = GeneratePath();
+        List<Vector2Int> generatedPath = PathGenerator.GeneratePath(Size, IndexesStart, IndexesEnd);
 
         TilesMap = Instantiate(TilesMapPrefab, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 1))
             .GetComponent<TilesMap>();
@@ -82,49 +83,5 @@ public class MapComponentsController : MonoBehaviour
         
     }
 
-    private List<Vector2Int> GeneratePath()
-    {
-        bool[,] visitedNode = new bool[Size, Size];
-        List<Vector2Int> pathNode = new List<Vector2Int>();
-
-        Vector2Int currentNode = indexesStart;
-        visitedNode[currentNode.x, currentNode.y] = true;
-        pathNode.Add(currentNode);
-
-        int[] rowNum = new[] { 1, 0, -1, 0 };
-        int[] columnNum = new[] { 0, 1, 0, -1 };
-
-        while (currentNode != indexesEnd)
-        {
-            List<Vector2Int> neighboringNode = new List<Vector2Int>();
-
-            for (int i = 0; i < rowNum.Length; i++)
-            {
-                Vector2Int node = new Vector2Int(currentNode.x + rowNum[i], currentNode.y + columnNum[i]);
-
-                if (visitedNode[node.x, node.y] == false && (node.x > 0 && node.x < Size - 1) && (node.y > 0 && node.y < Size - 1))
-                {
-                    neighboringNode.Add(node);
-                }
-            }
-
-            if (neighboringNode.Count > 0)
-            {
-                int index = Random.Range(0, neighboringNode.Count);
-                currentNode = neighboringNode[index];
-
-                visitedNode[currentNode.x, currentNode.y] = true;
-                pathNode.Add(currentNode);
-            }
-            else
-            {
-                pathNode.RemoveAt(pathNode.Count - 1);
-                currentNode = pathNode[pathNode.Count - 1];
-            }
-            
-        }
-
-        return pathNode;
-    }
-
+    
 }
