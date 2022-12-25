dfb6298f55ebf57ca30131d9fa3d9bbe894752d8
Alex <olexandrpasalsky@gmail.com>
2022-12-25 16:39:30 +0200

    fix bugs change scenes

--- TowerDefense/Assets/Scripts/MapComponent/MapComponentsController.cs
+++ TowerDefense/Assets/Scripts/MapComponent/MapComponentsController.cs
@@ -3,20 +3,15 @@ using UnityEngine;
 
 public class MapComponentsController : MonoBehaviour
 {
-    [Header("Prefabs")]
     [SerializeField]
-    private GameObject tilesMapPrefab;
+    private GameObject waveSpawner;
     [SerializeField]
-    private GameObject wayPointsPrefab;
+    private GameObject startBuilding;
     [SerializeField]
-    private GameObject startBuildingPrefab;
-    [SerializeField]
-    private GameObject endBuildingPrefab;
+    private GameObject endBuilding;
 
     private TilesMap tilesMap;
     private WayPoints wayPoints;
-    private Transform startBuilding;
-    private Transform endBuilding;
 
     [Header("Attributes")]
     [SerializeField]
@@ -26,6 +21,13 @@ public class MapComponentsController : MonoBehaviour
     [SerializeField]
     private int size = 16;
 
+    [Header("Prefabs")]
+    [SerializeField]
+    private GameObject tilesMapPrefab;
+    [SerializeField]
+    private GameObject wayPointsPrefab;
+
+
     void Start()
     {
         List<Vector2Int> generatedPath = PathGenerator.GeneratePath(size, indexesStart, indexesEnd);
@@ -34,12 +36,14 @@ public class MapComponentsController : MonoBehaviour
             .GetComponent<TilesMap>();
         tilesMap.Initialize(size, generatedPath);
 
-        startBuilding = Instantiate(startBuildingPrefab, new Vector3(5, (float)2.5, 5), new Quaternion(0, 0, 0, 1)).transform;
+        startBuilding.transform.SetPositionAndRotation(new Vector3(5, (float)2.5, 5), new Quaternion(0, 0, 0, 1));
 
-        endBuildingPrefab.transform.SetPositionAndRotation(new Vector3(70, (float)2.5, 70), new Quaternion(0, 0, 0, 1));
+        endBuilding.transform.SetPositionAndRotation(new Vector3(70, (float)2.5, 70), new Quaternion(0, 0, 0, 1));
 
         wayPoints = Instantiate(wayPointsPrefab, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 1))
             .GetComponent<WayPoints>();
         wayPoints.Initialize(generatedPath);
+
+        waveSpawner.GetComponent<WaveSpawner>().Initialize(startBuilding.transform);
     }
 }
